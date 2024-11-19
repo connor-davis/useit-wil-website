3567c420d5abd9ced33f1b1eb55766e7a5cd7a3e
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-11-19 15:46:04 +0200

    Fix download button

---
+++ Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace useitwebsite.Controllers
+{
+    public class FileController : Controller
+    {
+        // Action to serve the APK file
+        public IActionResult DownloadApp()
+        {
+            // Define the path to the file
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "resources", "UseIt-Blogs-Release.apk");
+
+            // Check if the file exists
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound(); // Return a 404 if the file is not found
+            }
+
+            // Return the file for download
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/vnd.android.package-archive", "UseIt-Blogs-Release.apk");
+        }
+    }
+}
